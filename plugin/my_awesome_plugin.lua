6e695cbd1df4a9d5dcaf99348fd8bf05f95505b7
Turiiya <34311583+tobealive@users.noreply.github.com>
2022-06-01 17:40:59 +0200

    Fix dependency check

--- plugin/my_awesome_plugin.lua
+++ plugin/my_awesome_plugin.lua
@@ -1,13 +1,13 @@
-if not vim.fn.has("nvim-0.7.0") then
+if vim.fn.has("nvim-0.7.0") == 0 then
   vim.api.nvim_err_writeln("my_awesome_plugin requires at least nvim-0.7.0.1")
   return
 end
 
 -- make sure this file is loaded only once
-if vim.g.loaded_my_awesome_plugin then
+if vim.g.loaded_my_awesome_plugin == 1 then
   return
 end
-vim.g.loaded_my_awesome_plugin = true
+vim.g.loaded_my_awesome_plugin = 1
 
 -- create any global command that does not depend on user setup
 -- usually it is better to define most commands/mappings in the setup function
@@ -18,3 +18,4 @@ vim.api.nvim_create_user_command(
   "MyPluginGenericGreet",
   my_awesome_plugin.generic_greet,
   {})
+
