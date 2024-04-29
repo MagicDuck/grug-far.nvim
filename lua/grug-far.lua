12c327e7267334afc5ef2deb4f7ddfa365bf333e
Stephan Badragan <stephan.badragan@everbridge.com>
2024-04-28 19:15:58 -0700

    fix opts

--- lua/grug-far.lua
+++ lua/grug-far.lua
@@ -14,7 +14,7 @@ function M.setup(options)
   -- avoid setting global values outside of this function. Global state
   -- mutations are hard to debug and test, so having them in a single
   -- function/module makes it easier to reason about all possible changes
-  M.options = with_defaults(options)
+  M.options = with_defaults(options or {})
 
   -- do here any startup your plugin needs, like creating commands and
   -- mappings that depend on values passed in options
