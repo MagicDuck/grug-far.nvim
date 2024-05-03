9dbdfd6421d48f67b3c0a1a868ce12f54f7d362f
Stephan Badragan <stephan.badragan@everbridge.com>
2024-05-03 10:55:04 -0700

    got the help text working

--- lua/grug-far.lua
+++ lua/grug-far.lua
@@ -16,32 +16,102 @@ function M.setup(options)
   -- function/module makes it easier to reason about all possible changes
   M.options = with_defaults(options or {})
 
-  -- do here any startup your plugin needs, like creating commands and
-  -- mappings that depend on values passed in options
-  vim.api.nvim_create_user_command("MyAwesomePluginGreet", M.greet, {})
+  M.namespace = vim.api.nvim_create_namespace('grug-far.nvim')
+  M.extmarkIds = {}
+
+  vim.api.nvim_create_user_command("GrugFar", M.grugFar, {})
 end
 
 function M.is_configured()
   return M.options ~= nil
 end
 
--- This is a function that will be used outside this plugin code.
--- Think of it as a public API
-function M.greet()
+function onBufferChange(params)
+  local buf = params.buf
+
+  local helpLine = unpack(vim.api.nvim_buf_get_lines(buf, 0, 1, false))
+  if #helpLine ~= 0 then
+    vim.api.nvim_buf_set_lines(buf, 0, 0, false, { "" })
+  end
+
+  local helpExtmarkPos = M.extmarkIds.help and
+    vim.api.nvim_buf_get_extmark_by_id(buf, M.namespace, M.extmarkIds.help, {}) or {}
+  if helpExtmarkPos[1] ~= 0 then
+    M.extmarkIds.help = vim.api.nvim_buf_set_extmark(buf, M.namespace, 0, 0, {
+      id = M.extmarkIds.help,
+      end_row = 0,
+      end_col = 0,
+      virt_text = {
+        { "Press g? for help", 'DiagnosticInfo' }
+      },
+      virt_text_pos = 'overlay'
+    })
+  end
+end
+
+-- public API
+function M.grugFar()
   if not M.is_configured() then
     return
   end
 
+  -- create split buffer
+  vim.cmd('vsplit')
+  local win = vim.api.nvim_get_current_win()
+  local buf = vim.api.nvim_create_buf(false, true)
+  vim.api.nvim_buf_set_name(buf, 'Grug Find and Replace')
+  vim.api.nvim_win_set_buf(win, buf)
+
+  vim.api.nvim_create_autocmd({ 'TextChanged', 'TextChangedI' }, {
+    buffer = buf,
+    callback = onBufferChange
+  })
+
+  -- add lines
+  -- vim.api.nvim_buf_set_lines(buf, 0, 0, false, {
+  --   "", -- search
+  --   "", -- replace
+  --   ""  -- flags
+  -- })
+  -- add virtual text
+  -- vim.api.nvim_buf_set_extmark(buf, M.namespace, 0, 0, {
+  --   end_row = 0,
+  --   end_col = 0,
+  --   virt_text = {
+  --     { " --help", 'DiagnosticInfo' }
+  --   },
+  --   virt_text_pos = 'overlay'
+  -- })
+  -- vim.api.nvim_buf_set_extmark(buf, M.namespace, 1, 0, {
+  --   end_row = 1,
+  --   end_col = 0,
+  --   virt_lines = {
+  --     { { "  Search", 'DiagnosticInfo' } },
+  --   },
+  --   virt_lines_leftcol = true,
+  --   virt_lines_above = true,
+  --   right_gravity = false
+  -- })
+  -- vim.api.nvim_buf_set_extmark(buf, M.namespace, 2, 0, {
+  --   end_row = 2,
+  --   end_col = 0,
+  --   -- TODO (sbadragan): create our own highlight group?
+  --   -- virt_lines = { { { "  Search" }, "Comment" } }
+  --   -- virt_text_pos = "eol"
+  --   virt_lines = {
+  --     { { "  Replace", 'DiagnosticInfo' } },
+  --   },
+  --   virt_lines_leftcol = true,
+  --   virt_lines_above = true,
+  --   right_gravity = false
+  -- })
+  -- TODO (sbadragan): update marks on TextChanged, TextChangedI
+
+  -- TODO (sbadragan): remove
   -- try to keep all the heavy logic on pure functions/modules that do not
   -- depend on Neovim APIs. This makes them easy to test
-  local greeting = my_cool_module.greeting(M.options.name)
-  print(greeting)
-end
-
--- Another function that belongs to the public API. This one does not depend on
--- user configuration
-function M.generic_greet()
-  print("Hello, unnamed friend!")
+  -- local greeting = my_cool_module.greeting(M.options.name)
+  -- print(greeting)
 end
 
 M.options = nil
--- plugin/grug-far.lua
+++ plugin/grug-far.lua
@@ -8,13 +8,3 @@ if vim.g.loaded_grug_far == 1 then
   return
 end
 vim.g.loaded_grug_far = 1
-
--- create any global command that does not depend on user setup
--- usually it is better to define most commands/mappings in the setup function
--- Be careful to not overuse this file!
-local grug_far = require("grug-far")
-
-vim.api.nvim_create_user_command(
-  "GrugFar",
-  grug_far.generic_greet,
-  {})
