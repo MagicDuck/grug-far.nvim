2844e0d32de84ad9de7501c973764241ac118ec0
Stephan Badragan <stephan.badragan@everbridge.com>
2024-05-05 13:29:59 -0700

    moved out args

--- lua/grug-far/rg/fetchResults.lua
+++ lua/grug-far/rg/fetchResults.lua
@@ -1,5 +1,5 @@
 local parseResults = require('grug-far/rg/parseResults')
-local colors = require('grug-far/rg/colors')
+local getArgs = require('grug-far/rg/getArgs')
 local uv = vim.loop
 
 local function fetchResults(params)
@@ -8,36 +8,9 @@ local function fetchResults(params)
   local on_error = params.on_error
   local inputs = params.inputs
 
-  local args = nil
   -- TODO (sbadragan): minimum search ?
   -- if yes control from higher level and only send here wehn > minsize
-  if #inputs.search > 0 then
-    args = { inputs.search }
-    if #inputs.replacement > 0 then
-      table.insert(args, '--replace=' .. inputs.replacement)
-    end
-    table.insert(args, '--heading')
-    if #inputs.filesGlob > 0 then
-      table.insert(args, '--glob=' .. inputs.filesGlob)
-    end
-
-    if #inputs.flags then
-      for flag in string.gmatch(inputs.flags, "%S+") do
-        table.insert(args, flag)
-      end
-    end
-
-    -- colors so that we can show nicer output
-    table.insert(args, '--color=ansi')
-    for k, v in pairs(colors.rg_colors) do
-      table.insert(args, '--colors=' .. k .. ':none')
-      table.insert(args, '--colors=' .. k .. ':fg:' .. v.rgb)
-    end
-
-    -- TODO (sbadragan): add option for extra rg args, or maybe just show number?
-    -- table.insert(args, '--line-number')
-  end
-
+  local args = getArgs(inputs)
   if not args then
     return
   end
---
+++ lua/grug-far/rg/getArgs.lua
@@ -0,0 +1,42 @@
+local colors = require('grug-far/rg/colors')
+
+local function getArgs(inputs)
+  local args = nil
+  -- TODO (sbadragan): minimum search ?
+  -- if yes control from higher level and only send here wehn > minsize
+  if #inputs.search == 0 then
+    return nil
+  end
+
+  args = { inputs.search }
+  if #inputs.replacement > 0 then
+    table.insert(args, '--replace=' .. inputs.replacement)
+  end
+
+  if #inputs.filesGlob > 0 then
+    table.insert(args, '--glob=' .. inputs.filesGlob)
+  end
+
+  table.insert(args, '--heading')
+
+  -- colors so that we can show nicer output
+  table.insert(args, '--color=ansi')
+  for k, v in pairs(colors.rg_colors) do
+    table.insert(args, '--colors=' .. k .. ':none')
+    table.insert(args, '--colors=' .. k .. ':fg:' .. v.rgb)
+  end
+
+  -- TODO (sbadragan): add option for extra rg args, or maybe just show number?
+  -- table.insert(args, '--line-number')
+
+  -- user provided flags should come last
+  if #inputs.flags then
+    for flag in string.gmatch(inputs.flags, "%S+") do
+      table.insert(args, flag)
+    end
+  end
+
+  return args
+end
+
+return getArgs
