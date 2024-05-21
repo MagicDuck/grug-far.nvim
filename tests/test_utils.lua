530be2ccf8580504c904d70b04821499cbe12e34
Stephan Badragan <stephan.badragan@everbridge.com>
2024-05-20 19:02:52 -0700

    fixing tests

---
+++ tests/test_utils.lua
@@ -0,0 +1,31 @@
+local utils = require('grug-far/utils')
+local expect = MiniTest.expect
+local new_set = MiniTest.new_set
+
+-- Actual tests definitions will go here
+describe('strEllideAfter', function()
+  it('returns empty string if n = 0', function()
+    expect.equality('', utils.strEllideAfter('hello world', 0))
+  end)
+  it('returns string if strlen < n', function()
+    expect.equality('hello world', utils.strEllideAfter('hello world', 20))
+  end)
+  it('returns ellided strign if strlen > n', function()
+    expect.equality('hello...', utils.strEllideAfter('hello world', 5))
+  end)
+  it('adds prefix if proficed', function()
+    expect.equality('say hello...', utils.strEllideAfter('hello world', 5, 'say '))
+  end)
+end)
+
+describe('strFindLast', function()
+  it('finds the last newline in a string when there are multiple', function()
+    expect.equality(8, utils.strFindLast('onentwo\nthree', '\n'))
+  end)
+  it('finds the last newline in a string when it is the last item', function()
+    expect.equality(8, utils.strFindLast('one\ntwo\n', '\n'))
+  end)
+  it('returns nil when there are none', function()
+    expect.equality(nil, utils.strFindLast('one_two_three', '\n'))
+  end)
+end)
