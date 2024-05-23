1789b72f33d2deac3ad2baf25db775e979072b09
Stephan Badragan <stephan.badragan@everbridge.com>
2024-05-23 02:35:35 -0700

    adding more tests

--- tests/test_search.lua
+++ tests/test_search.lua
@@ -18,12 +18,39 @@ local T = MiniTest.new_set({
 
 T['can search for some string'] = function()
   helpers.writeTestFiles({
-    file1 = [[ grug walks ]],
-    file2 = [[ 
+    { filename = 'file1', content = [[ grug walks ]] },
+    {
+      filename = 'file2',
+      content = [[ 
       grug talks and grug drinks
       then grug thinks
     ]],
+    },
   })
+
+  helpers.childRunGrugFar(child, {
+    prefills = { search = 'grug' },
+  })
+
+  helpers.childWaitForStatus(child, 'STATUS_SUCCESS')
+
+  expect.reference_screenshot(child.get_screenshot())
+  expect.reference_screenshot(screenshot.fromChildBufLines(child))
+end
+
+T['can search for some string with many matches'] = function()
+  local files = {}
+  for i = 1, 100 do
+    table.insert(files, {
+      filename = 'file_' .. i,
+      content = [[
+        grug walks many steps
+        grug talks and grug drinks
+        then grug thinks
+      ]],
+    })
+  end
+  helpers.writeTestFiles(files)
 
   helpers.childRunGrugFar(child, {
     prefills = { search = 'grug' },
