324db275c9bfe61e2395cf6ada9f6f9e163db74e
Stephan Badragan <stephan.badragan@everbridge.com>
2024-05-24 16:49:23 -0700

    modifying header

--- lua/grug-far/render/help.lua
+++ lua/grug-far/render/help.lua
@@ -1,57 +1,62 @@
 local opts = require('grug-far/opts')
 
----@param prefix string
 ---@param keymap KeymapDef
----@param mode string
 ---@return string | nil
-local function printMapping(prefix, keymap, mode)
-  local m = string.sub(mode, 1, 1)
-  local lhs = keymap[m]
-  return (lhs and #lhs > 0) and prefix .. ' ' .. lhs or nil
+local function getActionMappping(keymap)
+  local lhs = keymap.n
+  if not lhs or #lhs == 0 then
+    return nil
+  end
+  lhs = lhs
+    :gsub('<localleader>', vim.g.maplocalleader)
+    :gsub('<leader>', vim.g.mapleader == ' ' and '< >' or vim.g.mapleader)
+  return lhs
 end
 
+---@alias VirtText string[]
+
+--- gets help virtual text lines
+---@param context GrugFarContext
+---@return VirtText[][]
 local function getHelpVirtLines(context)
   local keymaps = context.options.keymaps
-  local mode = vim.fn.mode()
   local entries = vim.tbl_filter(function(m)
-    return m
+    return m.lhs
   end, {
-    printMapping('Replace', keymaps.replace, mode),
-    printMapping('Sync All', keymaps.syncLocations, mode),
-    printMapping('Sync Line', keymaps.syncLine, mode),
-    -- TODO (sbadragan): would need mappings for those
-    printMapping('History Open', keymaps.syncLocations, mode),
-    printMapping('History Add', keymaps.qflist, mode),
-
-    printMapping('Refresh', keymaps.refresh, mode),
-    printMapping('Quickfix', keymaps.qflist, mode),
-    printMapping('Goto', keymaps.gotoLocation, mode),
-    printMapping('Close', keymaps.close, mode),
+    { text = 'Replace', lhs = getActionMappping(keymaps.replace) },
+    { text = 'Sync All', lhs = getActionMappping(keymaps.syncLocations) },
+    { text = 'Sync Line', lhs = getActionMappping(keymaps.syncLine) },
+    { text = 'History Open', lhs = getActionMappping(keymaps.historyOpen) },
+    { text = 'History Add', lhs = getActionMappping(keymaps.historyAdd) },
+    { text = 'Refresh', lhs = getActionMappping(keymaps.refresh) },
+    { text = 'Goto', lhs = getActionMappping(keymaps.gotoLocation) },
+    { text = 'Quickfix', lhs = getActionMappping(keymaps.qflist) },
+    { text = 'Close', lhs = getActionMappping(keymaps.close) },
   })
 
   local maxEntryLen = 0
   for _, entry in ipairs(entries) do
-    if #entry > maxEntryLen then
-      maxEntryLen = #entry
+    local entrySize = #entry.text + #entry.lhs + 4
+    if entrySize > maxEntryLen then
+      maxEntryLen = entrySize
     end
   end
 
   local virt_lines = {}
   local sep = opts.getIcon('actionEntryBullet', context) or '| '
-  local max_width = 100
-  local entries_per_line = math.floor(max_width / maxEntryLen)
+  local headerMaxWidth = context.options.headerMaxWidth
+  local entries_per_line = math.floor(headerMaxWidth / maxEntryLen)
 
-  local line = ''
+  local line = {}
   for i, entry in ipairs(entries) do
-    local newLine = line .. sep .. string.format('%-' .. maxEntryLen .. 's', entry) .. '  '
-    if i == entries_per_line or i == #entries then
-      table.insert(virt_lines, { { newLine, 'GrugFarHelpHeader' } })
-      line = ''
-    else
-      line = newLine
+    table.insert(line, { sep .. entry.text .. ' ', 'GrugFarHelpHeader' })
+    table.insert(line, { entry.lhs, 'GrugFarHelpHeaderKey' })
+    table.insert(line, { string.rep(' ', maxEntryLen - #sep - #entry.text - #entry.lhs + 3) })
+    if i % entries_per_line == 0 or i == #entries then
+      table.insert(virt_lines, line)
+      line = {}
     end
   end
-
   return virt_lines
 end
 
@@ -60,9 +65,12 @@ end
 local function renderHelp(params, context)
   local buf = params.buf
 
+  local virt_lines = getHelpVirtLines(context)
   context.extmarkIds.help1 = vim.api.nvim_buf_set_extmark(buf, context.namespace, 0, 0, {
     id = context.extmarkIds.help1,
-    virt_lines = getHelpVirtLines(context),
+    virt_text = virt_lines[1],
+    virt_text_pos = 'overlay',
+    virt_lines = vim.list_slice(virt_lines, 2),
   })
 end
 
