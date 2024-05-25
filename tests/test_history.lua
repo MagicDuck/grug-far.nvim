0b2a747203f87b84a4bb8ca1c87fb30920fa486e
Stephan Badragan <stephan.badragan@everbridge.com>
2024-05-25 02:12:57 -0700

    finishing history test

--- tests/test_history.lua
+++ tests/test_history.lua
@@ -14,7 +14,7 @@ local T = MiniTest.new_set({
   },
 })
 
-T['can save and reload from history'] = function()
+T['can manually save and reload from history'] = function()
   helpers.writeTestFiles({
     { filename = 'file1.txt', content = [[ grug walks ]] },
     {
@@ -46,6 +46,62 @@ T['can save and reload from history'] = function()
   child.type_keys('<C-h>')
   helpers.childExpectScreenshot(child)
   helpers.childExpectBufLines(child)
+
+  child.type_keys(50, '<esc>16G', '<enter>')
+  vim.loop.sleep(50)
+  helpers.childExpectScreenshot(child)
+end
+
+T['auto-saves to history on replace'] = function()
+  helpers.writeTestFiles({
+    { filename = 'file1.txt', content = [[ grug walks ]] },
+    {
+      filename = 'file2.doc',
+      content = [[ 
+      grug talks and grug drinks
+      then grug thinks
+    ]],
+    },
+  })
+
+  helpers.childRunGrugFar(child, {
+    prefills = { search = 'grug', replacement = 'curly' },
+  })
+  helpers.childWaitForFinishedStatus(child)
+
+  child.type_keys('<C-enter>')
+  helpers.childWaitForUIVirtualText(child, 'replace completed!')
+  helpers.childExpectScreenshot(child)
+
+  vim.loop.sleep(50)
+  child.type_keys('<C-h>')
+  helpers.childExpectScreenshot(child)
+end
+
+T['auto-saves to history on sync all'] = function()
+  helpers.writeTestFiles({
+    { filename = 'file1.txt', content = [[ grug walks ]] },
+    {
+      filename = 'file2.doc',
+      content = [[ 
+      grug talks and grug drinks
+      then grug thinks
+    ]],
+    },
+  })
+
+  helpers.childRunGrugFar(child, {
+    prefills = { search = 'grug', replacement = 'curly' },
+  })
+  helpers.childWaitForFinishedStatus(child)
+
+  child.type_keys('<C-s>')
+  helpers.childWaitForUIVirtualText(child, 'sync completed!')
+  helpers.childExpectScreenshot(child)
+
+  vim.loop.sleep(50)
+  child.type_keys('<C-h>')
+  helpers.childExpectScreenshot(child)
 end
 
 return T
