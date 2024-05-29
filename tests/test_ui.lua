d942669ca87715dda40ef3c3322f9ef11d1f93c7
Stephan Badragan <stephan.badragan@everbridge.com>
2024-05-28 18:15:01 -0700

    adding a test for disabling keymaps

---
+++ tests/test_ui.lua
@@ -0,0 +1,27 @@
+local MiniTest = require('mini.test')
+local helpers = require('grug-far/test/helpers')
+
+---@type NeovimChild
+local child = MiniTest.new_child_neovim()
+
+local T = MiniTest.new_set({
+  hooks = {
+    pre_case = function()
+      helpers.initChildNeovim(child)
+    end,
+    -- Stop once all test cases are finished
+    post_once = child.stop,
+  },
+})
+
+T['can disable keymaps and they disappear from UI'] = function()
+  helpers.childRunGrugFar(child, {
+    keymaps = {
+      replace = false,
+      syncLine = '',
+    },
+  })
+  helpers.childExpectScreenshot(child)
+end
+
+return T
