a760f4f945ae8e628cf985342473059a9c195f0a
Stephan Badragan <stephan.badragan@everbridge.com>
2024-07-24 10:42:06 -0700

    adding text to flags placeholder to indicate users can put paths there

--- lua/grug-far/opts.lua
+++ lua/grug-far/opts.lua
@@ -140,7 +140,7 @@ M.defaultOptions = {
     search = 'ex: foo    foo([a-z0-9]*)    fun\\(',
     replacement = 'ex: bar    ${1}_foo    $$MY_ENV_VAR ',
     filesFilter = 'ex: *.lua     *.{css,js}    **/docs/*.md',
-    flags = 'ex: --help --ignore-case (-i) <relative-file-path> --replace= (empty replace) --multiline (-U)',
+    flags = 'ex: /foo/bar ./src/foo.lua --help --ignore-case (-i) --replace= (empty replace) --multiline (-U)',
   },
 
   -- strings to auto-fill in each input area at start
