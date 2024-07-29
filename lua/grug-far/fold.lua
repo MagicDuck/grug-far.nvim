1910b90bbe3cce6325bc7363220e8c7435119daf
Stephan Badragan <stephan.badragan@everbridge.com>
2024-07-29 14:12:41 -0700

    fixing it u

--- lua/grug-far/fold.lua
+++ lua/grug-far/fold.lua
@@ -1,11 +1,23 @@
 local M = {}
 
+---@param line string
+---@return boolean
+local function isPartOfFold(line)
+  return line and #line > 0 and (line:match('^(%d+:%d+:)') or line:match('^(%d+%-)'))
+end
+
+--- gets fold level of line at given number
+---@param lnum integer
+---@return integer
 M.getFoldLevel = function(lnum)
   local line = vim.fn.getline(lnum)
-  if line and #line > 0 and (line:match('^(%d+:%d+:)') or line:match('^(%d+%-)')) then
+  -- TODO (sbadragan): need this?
+  -- local nextline = vim.fn.getline(lnum + 1)
+
+  -- if isPartOfFold(line) or isPartOfFold(nextline) then
+  if isPartOfFold(line) then
     return 1
   end
-
   return 0
 end
 
@@ -14,24 +26,9 @@ end
 M.updateFolds = function(buf)
   local win = vim.fn.bufwinid(buf)
   if win ~= -1 then
-    -- local cursor_row = unpack(vim.api.nvim_win_get_cursor(0))
     local cursor = vim.api.nvim_win_get_cursor(win)
     vim.fn.win_execute(win, 'normal zx')
     vim.api.nvim_win_set_cursor(win, cursor)
-
-    -- if win == vim.api.nvim_get_current_win() then
-    --   if vim.fn.mode():lower():find('i') ~= nil then
-    --     local key = vim.api.nvim_replace_termcodes('<C-o>zx', true, false, true)
-    --     vim.api.nvim_feedkeys(key, 'i', true)
-    --   end
-    -- else
-    --   vim.fn.win_execute(win, 'normal zx')
-    -- end
-
-    -- vim.fn.win_execute(win, 'normal zx')
-    -- if vim.fn.mode():lower():find('v') ~= nil then
-    -- vim.fn.win_execute(win, 'startinsert!')
-    -- end
   end
 end
 
