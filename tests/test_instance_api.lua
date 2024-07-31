0da15c79fdfb6fadd0d219f3c078a8504c65cec8
Stephan Badragan <stephan.badragan@everbridge.com>
2024-07-30 19:53:12 -0700

    adding unit tests and renaming some

---
+++ tests/test_instance_api.lua
@@ -0,0 +1,98 @@
+local MiniTest = require('mini.test')
+local expect = MiniTest.expect
+local helpers = require('grug-far/test/helpers')
+
+---@type NeovimChild
+local child = MiniTest.new_child_neovim()
+
+local T = MiniTest.new_set({
+  hooks = {
+    pre_case = function()
+      helpers.initChildNeovim(child)
+    end,
+    -- Stop once all test cases are finished
+    post_once = child.stop,
+  },
+})
+
+T['can create instance, open, close and kill it'] = function()
+  -- create it
+  local instanceName = helpers.childRunGrugFar(child, { staticTitle = 'Find and Replace' })
+  helpers.childWaitForScreenshotText(child, 'Search:')
+  helpers.childExpectScreenshot(child)
+
+  -- check exists and is open
+  expect.equality(child.lua_get('GrugFar.has_instance(...)', { instanceName }), true)
+  expect.equality(child.lua_get('GrugFar.is_instance_open(...)', { instanceName }), true)
+
+  -- close
+  child.lua('GrugFar.close_instance(...)', { instanceName })
+  helpers.childWaitForScreenshotNotContainingText(child, 'Search:')
+  helpers.childExpectScreenshot(child)
+
+  -- check exists and is not open
+  expect.equality(child.lua_get('GrugFar.has_instance(...)', { instanceName }), true)
+  expect.equality(child.lua_get('GrugFar.is_instance_open(...)', { instanceName }), false)
+
+  -- open
+  child.lua('GrugFar.open_instance(...)', { instanceName })
+  helpers.childWaitForScreenshotText(child, 'Search:')
+  helpers.childExpectScreenshot(child)
+
+  -- check exists and is open
+  expect.equality(child.lua_get('GrugFar.has_instance(...)', { instanceName }), true)
+  expect.equality(child.lua_get('GrugFar.is_instance_open(...)', { instanceName }), true)
+
+  -- open again (should do nothing)
+  child.lua('GrugFar.open_instance(...)', { instanceName })
+  helpers.childWaitForScreenshotText(child, 'Search:')
+  helpers.childExpectScreenshot(child)
+
+  -- check exists and is open
+  expect.equality(child.lua_get('GrugFar.has_instance(...)', { instanceName }), true)
+  expect.equality(child.lua_get('GrugFar.is_instance_open(...)', { instanceName }), true)
+
+  -- kill
+  child.lua('GrugFar.kill_instance(...)', { instanceName })
+  helpers.childWaitForScreenshotNotContainingText(child, 'Search:')
+  helpers.childExpectScreenshot(child)
+
+  -- check not exists and is not open
+  expect.equality(child.lua_get('GrugFar.has_instance(...)', { instanceName }), false)
+  expect.equality(child.lua_get('GrugFar.is_instance_open(...)', { instanceName }), false)
+end
+
+T['can update instance prefills'] = function()
+  helpers.writeTestFiles({
+    { filename = 'file1.txt', content = [[ 
+       grug walks
+       then grug swims
+      ]] },
+    {
+      filename = 'file2.doc',
+      content = [[ 
+      grug talks and grug drinks
+      then grug thinks
+    ]],
+    },
+  })
+
+  -- create it
+  local instanceName = helpers.childRunGrugFar(child, {
+    staticTitle = 'Find and Replace',
+    prefills = { search = 'grug' },
+  })
+  helpers.childWaitForScreenshotText(child, '5 matches in 2 files')
+
+  -- prefill while keeping existing
+  child.lua('GrugFar.update_instance_prefills(...)', { instanceName, { paths = 'file1.txt' } })
+  helpers.childWaitForScreenshotText(child, '2 matches in 1 files')
+  helpers.childExpectScreenshot(child)
+
+  -- prefill while clearing old
+  child.lua('GrugFar.update_instance_prefills(...)', { instanceName, { search = 'walks' }, true })
+  helpers.childWaitForScreenshotText(child, '1 matches in 1 files')
+  helpers.childExpectScreenshot(child)
+end
+
+return T
