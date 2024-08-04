6eb2524d3e925e0e0f435129c67147f7bf10e678
Stephan Badragan <stephan.badragan@everbridge.com>
2024-08-04 00:08:17 -0700

    got replacement working

--- lua/grug-far/engine/astgrep.lua
+++ lua/grug-far/engine/astgrep.lua
@@ -1,7 +1,43 @@
-local utils = require('grug-far/utils')
 local fetchCommandOutput = require('grug-far/engine/fetchCommandOutput')
 local getArgs = require('grug-far/engine/astgrep/getArgs')
 local parseResults = require('grug-far/engine/astgrep/parseResults')
+
+--- decodes streamed json matches, appending to given table
+---@param matches AstgrepMatch[]
+---@param data string
+local function json_decode_matches(matches, data)
+  local json_lines = vim.split(data, '\n')
+  for _, json_line in ipairs(json_lines) do
+    if #json_line > 0 then
+      local match = vim.json.decode(json_line)
+      table.insert(matches, match)
+    end
+  end
+end
+
+--- splits off matches corresponding to the last file
+---@param matches AstgrepMatch[]
+---@return AstgrepMatch[] before, AstgrepMatch[] after
+local function split_last_file_matches(matches)
+  local end_index = 0
+  for i = #matches - 1, 1, -1 do
+    if matches[i].file ~= matches[i + 1].file then
+      end_index = i
+      break
+    end
+  end
+
+  local before = {}
+  for i = 1, end_index do
+    table.insert(before, matches[i])
+  end
+  local after = {}
+  for i = end_index + 1, #matches do
+    table.insert(after, matches[i])
+  end
+
+  return before, after
+end
 
 ---@type GrugFarEngine
 local AstgrepEngine = {
@@ -14,15 +50,27 @@ local AstgrepEngine = {
     local args = getArgs(params.inputs, params.options, extraArgs)
 
     local hadOutput = false
+    local matches = {}
     return fetchCommandOutput({
       cmd_path = params.options.engines.astgrep.path,
       args = args,
       options = params.options,
       on_fetch_chunk = function(data)
         hadOutput = true
-        params.on_fetch_chunk(parseResults(data))
+        json_decode_matches(matches, data)
+        -- note: we split off last file matches to ensure all matches for a file are processed
+        -- at once. This helps with applying replacements
+        local before, after = split_last_file_matches(matches)
+        matches = after
+        params.on_fetch_chunk(parseResults(before))
       end,
       on_finish = function(status, errorMessage)
+        if #matches > 0 then
+          -- do the last few
+          params.on_fetch_chunk(parseResults(matches))
+          matches = {}
+        end
+
         -- give the user more feedback when there are no matches
         if status == 'success' and not (errorMessage and #errorMessage > 0) and not hadOutput then
           status = 'error'
