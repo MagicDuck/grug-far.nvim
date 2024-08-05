10fd36710c5088862f085a494f312c7eed582cc4
Stephan Badragan <stephan.badragan@everbridge.com>
2024-08-05 11:18:16 -0700

    added tests for quickfix

---
+++ tests/astgrep/test_quickfix.lua
@@ -0,0 +1,48 @@
+local MiniTest = require('mini.test')
+local helpers = require('grug-far/test/helpers')
+local keymaps = helpers.getKeymaps()
+
+---@type NeovimChild
+local child = MiniTest.new_child_neovim()
+
+local T = MiniTest.new_set({
+  hooks = {
+    pre_case = function()
+      helpers.initChildNeovim(child)
+    end,
+    -- Stop once all test cases are finished
+    post_once = child.stop,
+  },
+})
+
+T['can open quickfix list'] = function()
+  helpers.writeTestFiles({
+    {
+      filename = 'file1.js',
+      content = [[ 
+    if (grug || another_thing) {
+      console.log(grug)
+    }
+      ]],
+    },
+    {
+      filename = 'file2.ts',
+      content = [[ 
+    if (grug || talks) {
+      grug.walks(talks)
+    }
+    ]],
+    },
+  })
+
+  helpers.childRunGrugFar(child, {
+    prefills = { search = 'grug' },
+  })
+  helpers.childWaitForFinishedStatus(child)
+
+  child.type_keys('<esc>' .. keymaps.qflist.n)
+  helpers.childWaitForScreenshotText(child, 'Quickfix List')
+  helpers.childExpectScreenshot(child)
+end
+
+return T
