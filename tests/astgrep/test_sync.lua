db239380edc8dc1c9a8518635deb16f837756d68
Stephan Badragan <stephan.badragan@everbridge.com>
2024-08-05 14:36:16 -0700

    added sync

---
+++ tests/astgrep/test_sync.lua
@@ -0,0 +1,88 @@
+local MiniTest = require('mini.test')
+local helpers = require('grug-far/test/helpers')
+local keymaps = helpers.getKeymaps()
+
+---@type NeovimChild
+local child = MiniTest.new_child_neovim()
+
+local T = MiniTest.new_set({
+  hooks = {
+    pre_case = function()
+      helpers.initChildNeovim(child)
+    end,
+    -- Stop once all test cases are finished
+    post_once = child.stop,
+  },
+})
+
+T['sync all is not supported'] = function()
+  helpers.writeTestFiles({
+    {
+      filename = 'file1.js',
+      content = [[ 
+    if (grug || another_thing) {
+      console.log(grug)
+    }
+      ]],
+    },
+    {
+      filename = 'file2.ts',
+      content = [[ 
+    if (grug || talks) {
+      grug.walks(talks)
+    }
+    ]],
+    },
+  })
+
+  helpers.childRunGrugFar(child, {
+    engine = 'astgrep',
+    prefills = { search = 'grug' },
+  })
+  helpers.childWaitForFinishedStatus(child)
+
+  child.type_keys(10, '<esc>10G', 'dd')
+  child.type_keys(10, 'A', ' a deep depth indeed!')
+
+  child.type_keys('<esc>' .. keymaps.syncLocations.n)
+
+  helpers.childWaitForUIVirtualText(child, 'sync operation not suppored')
+  helpers.childExpectScreenshot(child)
+end
+
+T['sync line is not supported'] = function()
+  helpers.writeTestFiles({
+    {
+      filename = 'file1.js',
+      content = [[ 
+    if (grug || another_thing) {
+      console.log(grug)
+    }
+      ]],
+    },
+    {
+      filename = 'file2.ts',
+      content = [[ 
+    if (grug || talks) {
+      grug.walks(talks)
+    }
+    ]],
+    },
+  })
+
+  helpers.childRunGrugFar(child, {
+    engine = 'astgrep',
+    prefills = { search = 'grug' },
+  })
+  helpers.childWaitForFinishedStatus(child)
+
+  child.type_keys(10, '<esc>10G', 'dd')
+  child.type_keys(10, 'A', ' a deep depth indeed!')
+
+  child.type_keys('<esc>' .. keymaps.syncLine.n)
+
+  helpers.childWaitForUIVirtualText(child, 'sync operation not suppored')
+  helpers.childExpectScreenshot(child)
+end
+
+return T
