d784038a28e1e0bb5b32d96c279430b295371a5f
Stephan Badragan <stephan.badragan@everbridge.com>
2024-08-11 13:19:20 -0700

    removing duplicated function

--- lua/grug-far/engine/astgrep.lua
+++ lua/grug-far/engine/astgrep.lua
@@ -2,30 +2,13 @@ local utils = require('grug-far/utils')
 local search = require('grug-far/engine/astgrep/search')
 local replace = require('grug-far/engine/astgrep/replace')
 
---- is doing a search with replacement?
----@param args string[]?
----@return boolean
-local function isSearchWithReplacement(args)
-  if not args then
-    return false
-  end
-
-  for i = 1, #args do
-    if vim.startswith(args[i], '--rewrite=') or args[i] == '--rewrite' or args[i] == '-r' then
-      return true
-    end
-  end
-
-  return false
-end
-
 ---@type GrugFarEngine
 local AstgrepEngine = {
   type = 'astgrep',
 
   isSearchWithReplacement = function(inputs, options)
     local args = search.getSearchArgs(inputs, options)
-    return isSearchWithReplacement(args)
+    return search.isSearchWithReplacement(args)
   end,
 
   search = search.search,
