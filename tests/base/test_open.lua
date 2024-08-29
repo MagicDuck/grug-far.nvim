6f1979e9dbbef530d48b8103efa59da9d6d46dd0
MagicDuck <sbadragan@gmail.com>
2024-08-28 23:16:26 -0700

    adding open test

--- tests/base/test_open.lua
+++ tests/base/test_open.lua
@@ -38,7 +38,7 @@ T['can open a given location'] = function()
 
   child.type_keys('<esc>11G')
   child.type_keys('<esc>' .. keymaps.openLocation.n)
-  helpers.childWaitForScreenshotText(child, 'Top file1.txt')
+  helpers.childWaitForScreenshotText(child, '3,13')
   helpers.childExpectScreenshot(child)
 end
 
@@ -64,7 +64,7 @@ T['can open a location with count'] = function()
   helpers.childWaitForFinishedStatus(child)
 
   child.type_keys('<esc>2' .. keymaps.openLocation.n)
-  helpers.childWaitForScreenshotText(child, 'Top file1.txt')
+  helpers.childWaitForScreenshotText(child, '3,13')
   helpers.childExpectScreenshot(child)
 end
 
@@ -149,6 +149,32 @@ T['can open prev location'] = function()
   child.type_keys(keymaps.openPrevLocation.n)
   vim.uv.sleep(100)
   helpers.childWaitForScreenshotText(child, '2,8')
+  helpers.childExpectScreenshot(child)
+end
+
+T['can open a given location when only window in tabpage'] = function()
+  helpers.writeTestFiles({
+    { filename = 'file1.txt', content = [[ 
+       grug walks
+       then grug swims
+      ]] },
+    {
+      filename = 'file2.doc',
+      content = [[ 
+      grug talks and grug drinks
+      then grug thinks
+    ]],
+    },
+  })
+
+  helpers.childRunGrugFar(child, {
+    prefills = { search = 'grug' },
+  })
+  helpers.childWaitForFinishedStatus(child)
+
+  child.type_keys('<esc>11G')
+  child.type_keys('<esc>' .. keymaps.openLocation.n)
+  helpers.childWaitForScreenshotText(child, '3,13')
   helpers.childExpectScreenshot(child)
 end
 
