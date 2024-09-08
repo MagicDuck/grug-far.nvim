3792e6e2635b658512c8b79048d9f49a12e8c7a7
MagicDuck <sbadragan@gmail.com>
2024-09-08 11:10:27 -0700

    switching to strategy of doing a bunch at a time

--- lua/grug-far/engine/ripgrep/search.lua
+++ lua/grug-far/engine/ripgrep/search.lua
@@ -4,7 +4,6 @@ local getRgVersion = require('grug-far/engine/ripgrep/getRgVersion')
 local parseResults = require('grug-far/engine/ripgrep/parseResults')
 local getArgs = require('grug-far/engine/ripgrep/getArgs')
 local colors = require('grug-far/engine/ripgrep/colors')
-local MatchReplacer = require('grug-far/engine/ripgrep/MatchReplacer')
 local uv = vim.uv
 
 local M = {}
@@ -13,6 +12,7 @@ local M = {}
 ---@param inputs GrugFarInputs
 ---@param options GrugFarOptions
 ---@return string[]?
+-- TODO (sbadragan): update?
 function M.getSearchArgs(inputs, options)
   local extraArgs = { '--color=ansi' }
   for k, v in pairs(colors.rg_colors) do
@@ -40,17 +40,95 @@ function M.isSearchWithReplacement(args)
   return false
 end
 
+---@class ResultsWithReplaceDiffParams
+-- TODO (sbadragan): fix this up
+---@field json_data any
+---@field options GrugFarOptions
+---@field inputs GrugFarInputs
+---@field on_finish fun(status: GrugFarStatus, errorMesage: string?, results: ParsedResultsData?)
+
 --- adds results of doing a replace to results of doing a search
----@param params { data: string, matchReplacer: MatchReplacer, on_finish: fun(results: ParsedResultsData)}
+---@param params ResultsWithReplaceDiffParams
+---@return fun()? abort
 local function getResultsWithReplaceDiff(params)
-  local data = params.data
-  params.matchReplacer:get_replaced_lines(data, function(replaced_lines)
-    -- TODO (sbadragan): need to add diff bar sign
-    local results = parseResults(data .. '\n' .. replaced_lines, false)
-    params.on_finish(results)
-  end)
+  local json_data = params.json_data
+  local matches_for_replacement = {}
+  for _, json_result in ipairs(json_data) do
+    if json_result.type == 'match' then
+      for _, submatch in ipairs(json_result.data.submatches) do
+        table.insert(matches_for_replacement, submatch.match.text)
+      end
+    end
+  end
+  if #matches_for_replacement == 0 then
+    params.on_finish('success', nil, nil)
+    return
+  end
+
+  local stdin = uv.new_pipe()
+  local replaced_matches_text = nil
+  local match_separator = '\029'
+
+  local replaceInputs = vim.deepcopy(params.inputs)
+  replaceInputs.paths = ''
+  replaceInputs.filesFilter = ''
+  local replaceArgs = getArgs(
+    replaceInputs,
+    params.options,
+    { '--color=never', '--no-heading', '--no-line-number', '--no-column', '--no-filename' }
+  ) --[[ @as string[] ]]
+
+  local abort = fetchCommandOutput({
+    cmd_path = params.options.engines.ripgrep.path,
+    args = replaceArgs,
+    stdin = stdin,
+    on_fetch_chunk = function(data)
+      replaced_matches_text = replaced_matches_text and replaced_matches_text .. data or data
+    end,
+    on_finish = function(status, errorMessage)
+      -- TODO (sbadragan): on abort case handle
+      if status == 'success' then
+        ---@cast replaced_matches_text string
+        local replaced_matches = vim.split(replaced_matches_text, match_separator)
+        local i = 0
+        for _, json_result in ipairs(json_data) do
+          if json_result.type == 'match' then
+            for _, submatch in ipairs(json_result.data.submatches) do
+              i = i + 1
+              submatch.replacement = { text = replaced_matches[i] }
+            end
+          end
+        end
+
+        -- TODO (sbadragan): need to parse those properly
+        local results = parseResults('hello', false)
+        params.on_finish(status, nil, results)
+        P('gets here 1')
+      else
+        P('gets here 2')
+        params.on_finish(status, errorMessage)
+      end
+    end,
+  })
+
+  uv.write(
+    stdin,
+    vim.fn.join(matches_for_replacement, match_separator) .. match_separator,
+    function()
+      uv.shutdown(stdin)
+    end
+  )
+  print('run with', vim.inspect(matches_for_replacement))
+
+  return abort
 end
 
+-- TODO (sbadragan): bug in history when not on lines
+-- [C]: in function 'nvim_buf_get_lines'
+-- /opt/repos/grug-far.nvim/lua/grug-far/utils.lua:251: in function 'ensureBufTopEmptyLines'
+-- ...repos/grug-far.nvim/lua/grug-far/actions/historyOpen.lua:94: in function 'renderHistoryBuffer'
+-- ...repos/grug-far.nvim/lua/grug-far/actions/historyOpen.lua:190: in function <...repos/grug-far.nvim/lua/grug-far/actions/historyOpen.lua:189>
+--
 ---@param args string[]?
 ---@return string[]? newArgs
 local function stripReplaceArgs(args)
@@ -73,6 +151,112 @@ local function stripReplaceArgs(args)
   return newArgs
 end
 
+---@class RipgrepEngineSearchParams
+---@field args string[]?
+---@field options GrugFarOptions
+---@field inputs GrugFarInputs
+---@field on_fetch_chunk fun(data: ParsedResultsData)
+---@field on_finish fun(status: GrugFarStatus, errorMesage: string?, customActionMessage: string?)
+---@field isSearchWithReplace boolean
+
+--- runs search
+---@param params RipgrepEngineSearchParams
+---@return fun()? abort, string[]? effectiveArgs
+local function run_search(params)
+  return fetchCommandOutput({
+    cmd_path = params.options.engines.ripgrep.path,
+    args = params.args,
+    on_fetch_chunk = function(data)
+      params.on_fetch_chunk(parseResults(data, params.isSearchWithReplace))
+    end,
+    on_finish = function(status, errorMessage)
+      if status == 'error' and errorMessage and #errorMessage == 0 then
+        errorMessage = 'no matches'
+      end
+      params.on_finish(status, errorMessage)
+    end,
+  })
+end
+
+--- runs search with replace diff
+---@param params RipgrepEngineSearchParams
+---@return fun()? abort, string[]? effectiveArgs
+local function run_search_with_replace_diff(params)
+  local abortSearch = nil
+  local effectiveArgs = nil
+  local processingQueue = nil
+
+  local abort = function()
+    if processingQueue then
+      processingQueue:stop()
+    end
+    if abortSearch then
+      abortSearch()
+    end
+  end
+
+  local searchArgs = stripReplaceArgs(params.args)
+  if searchArgs then
+    -- TODO (sbadragan): put this into getSearchArgs??
+    table.insert(searchArgs, '--json')
+  end
+
+  processingQueue = ProcessingQueue.new(function(json_data, on_done)
+    getResultsWithReplaceDiff({
+      json_data = json_data,
+      inputs = params.inputs,
+      options = params.options,
+      on_finish = function(status, errorMessage, results)
+        if status == 'success' then
+          if results then
+            params.on_fetch_chunk(results)
+          end
+          on_done()
+        else
+          abort()
+          P('finishing here 1')
+          params.on_finish(status, errorMessage)
+        end
+      end,
+    })
+  end)
+
+  abortSearch, effectiveArgs = fetchCommandOutput({
+    cmd_path = params.options.engines.ripgrep.path,
+    args = searchArgs,
+    on_fetch_chunk = function(data)
+      local json_lines = vim.split(data, '\n')
+      local json_data = {}
+      for _, json_line in ipairs(json_lines) do
+        if #json_line > 0 then
+          local entry = vim.json.decode(json_line)
+          table.insert(json_data, entry)
+        end
+      end
+
+      processingQueue:push(json_data)
+    end,
+    on_finish = function(status, errorMessage)
+      if status == 'error' and errorMessage and #errorMessage == 0 then
+        errorMessage = 'no matches'
+      end
+      if status == 'success' then
+        processingQueue:on_finish(function()
+          processingQueue:stop()
+          P('finishing here 2')
+          params.on_finish(status, errorMessage)
+        end)
+      else
+        P('finishing here 2')
+        processingQueue:stop()
+        params.on_finish(status, errorMessage)
+      end
+    end,
+  })
+
+  return abort, effectiveArgs
+end
+
 --- does search
 ---@param params EngineSearchParams
 ---@return fun()? abort, string[]? effectiveArgs
@@ -90,84 +274,27 @@ function M.search(params)
 
   local args = M.getSearchArgs(params.inputs, params.options)
   local isSearchWithReplace = M.isSearchWithReplacement(args)
-
-  local on_fetch_chunk = function(data)
-    params.on_fetch_chunk(parseResults(data, isSearchWithReplace))
-  end
-
-  local on_finish = function(status, errorMessage)
-    if status == 'error' and errorMessage and #errorMessage == 0 then
-      errorMessage = 'no matches'
-    end
-    params.on_finish(status, errorMessage)
-  end
-
-  local cleanup = nil
-  local abort = nil
-  local effectiveArgs = nil
-
   local showDiff = isSearchWithReplace and options.engines.ripgrep.showDiffOnReplace
+
   if showDiff then
-    -- TODO (sbadragan): launch rg stdin process. If it fails for some reason, propagate the error
-    args = stripReplaceArgs(args)
-
-    local replaceInputs = vim.deepcopy(params.inputs)
-    replaceInputs.paths = ''
-    replaceInputs.filesFilter = ''
-    local replaceArgs = M.getSearchArgs(replaceInputs, params.options) --[[ @as string[] ]]
-    local processingQueue = nil
-    local matchReplacer = MatchReplacer.new(params.options, replaceArgs, function(errorMessage)
-      if processingQueue then
-        processingQueue:stop()
-      end
-      if abort then
-        abort()
-      end
-      params.on_finish('error', errorMessage)
-    end)
-
-    processingQueue = ProcessingQueue.new(function(data, on_done)
-      getResultsWithReplaceDiff({
-        data = data,
-        matchReplacer = matchReplacer,
-        on_finish = function(results)
-          params.on_fetch_chunk(results)
-          on_done()
-        end,
-      })
-    end)
-
-    on_fetch_chunk = function(data)
-      processingQueue:push(data)
-    end
-
-    on_finish = function(status, errorMessage)
-      if status == 'error' and errorMessage and #errorMessage == 0 then
-        errorMessage = 'no matches'
-      end
-      if status ~= 'success' then
-        processingQueue:stop()
-        -- TODO (sbadragan): turn back?
-        -- matchReplacer:destroy()
-        params.on_finish(status, errorMessage)
-      else
-        processingQueue:on_finish(function()
-          processingQueue:stop()
-          -- matchReplacer:destroy()
-          params.on_finish(status, errorMessage)
-        end)
-      end
-    end
+    return run_search_with_replace_diff({
+      options = options,
+      inputs = params.inputs,
+      args = args,
+      on_fetch_chunk = params.on_fetch_chunk,
+      on_finish = params.on_finish,
+      isSearchWithReplace = isSearchWithReplace,
+    })
+  else
+    return run_search({
+      options = options,
+      inputs = params.inputs,
+      args = args,
+      on_fetch_chunk = params.on_fetch_chunk,
+      on_finish = params.on_finish,
+      isSearchWithReplace = isSearchWithReplace,
+    })
   end
-
-  abort, effectiveArgs = fetchCommandOutput({
-    cmd_path = params.options.engines.ripgrep.path,
-    args = args,
-    on_fetch_chunk = on_fetch_chunk,
-    on_finish = on_finish,
-  })
-
-  return abort, effectiveArgs
 end
 
 return M
