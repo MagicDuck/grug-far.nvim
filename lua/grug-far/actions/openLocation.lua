e5e87d7ea8403f4fdd50100e09c4b97f468a5658
MagicDuck <sbadragan@gmail.com>
2024-09-22 15:20:08 -0700

    fixing buffer polution when previewing

--- lua/grug-far/actions/openLocation.lua
+++ lua/grug-far/actions/openLocation.lua
@@ -74,17 +74,46 @@ local function open(params)
 
   vim.api.nvim_command([[execute "normal! m` "]])
 
-  ---@diagnostic disable-next-line
-  local bufnr = vim.fn.bufnr(location.filename)
   local targetWin = utils.getOpenTargetWin(context, buf)
-
-  if bufnr == -1 then
-    vim.fn.win_execute(targetWin, 'e ' .. vim.fn.fnameescape(location.filename), true)
+  local targetBuf = vim.fn.bufnr(location.filename)
+  if targetBuf == -1 then
+    vim.fn.win_execute(targetWin, 'e! ' .. vim.fn.fnameescape(location.filename), true)
+    targetBuf = vim.api.nvim_win_get_buf(targetWin)
   else
-    vim.api.nvim_win_set_buf(targetWin, bufnr)
+    vim.api.nvim_win_set_buf(targetWin, targetBuf)
   end
 
-  vim.api.nvim_win_set_cursor(
+  vim.api.nvim_set_option_value('buflisted', true, { buf = targetBuf })
+
+  if not vim.b[targetBuf].__grug_far_was_visited then
+    local bufHiddenAutocmdId, bufEnterAutocmdId
+    bufHiddenAutocmdId = vim.api.nvim_create_autocmd({ 'BufHidden' }, {
+      buffer = targetBuf,
+      callback = function()
+        vim.api.nvim_del_autocmd(bufHiddenAutocmdId)
+        vim.api.nvim_del_autocmd(bufEnterAutocmdId)
+        vim.api.nvim_set_option_value('buflisted', false, { buf = targetBuf })
+        vim.schedule(function()
+          -- note: using bdelete! instead of nvim_buf_delete or bwipeout!
+          -- due to an issue in nvim similar to this issue described in oil:
+          -- https://github.com/stevearc/oil.nvim/issues/435
+          ---@diagnostic disable-next-line: param-type-mismatch
+          pcall(vim.cmd, 'bdelete! ' .. targetBuf)
+        end)
+      end,
+    })
+    bufEnterAutocmdId = vim.api.nvim_create_autocmd({ 'WinEnter' }, {
+      buffer = targetBuf,
+      callback = function()
+        vim.b[targetBuf].__grug_far_was_visited = true
+        vim.api.nvim_del_autocmd(bufHiddenAutocmdId)
+        vim.api.nvim_del_autocmd(bufEnterAutocmdId)
+      end,
+    })
+  end
+
+  pcall(
+    vim.api.nvim_win_set_cursor,
     targetWin,
     { location.lnum or 1, location.col and location.col - 1 or 0 }
   )
