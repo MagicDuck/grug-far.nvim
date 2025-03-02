b97ffbaaf76c74822701d1409b61cc26b914daa3
Jaye Heffernan <jpeheffernan@gmail.com>
2025-03-02 16:04:25 +1100

    Handle funny error with one search char

--- lua/grug-far/engine/astgrep/search.lua
+++ lua/grug-far/engine/astgrep/search.lua
@@ -135,7 +135,11 @@ function M.search(params)
     )
     return
   end
+  local isRuleMode = params.inputs.rules ~= nil
   local numSearchChars = #params.inputs.search
+  if isRuleMode then
+    numSearchChars = #params.inputs.rules
+  end
   if numSearchChars > 0 and numSearchChars < (params.options.minSearchChars or 1) then
     params.on_finish(
       'success',
