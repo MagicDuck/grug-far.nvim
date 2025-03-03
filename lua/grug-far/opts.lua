09bf2218df56c7a70b11c5f4d7e3b647bf1ab394
Jaye Heffernan <jpeheffernan@gmail.com>
2025-03-03 15:03:36 +1100

    Tweak placeholder val

--- lua/grug-far/opts.lua
+++ lua/grug-far/opts.lua
@@ -123,7 +123,7 @@ M.defaultOptions = {
         --  rules would normally be multi-line, but we don't support multi-line
         --  placeholders. rules is filled with a default-value though, so it's
         --  rare to see it empty
-        rules = 'id: my_rule_1',
+        rules = 'e.g. id: my_rule_1 \\n language: lua\\nrule: \\n  pattern: await $A',
         filesFilter = 'e.g. *.lua   *.{css,js}   **/docs/*.md   (specify one per line, filters via ripgrep)',
         flags = 'e.g. --help (-h) --debug-query=ast --strictness=<STRICTNESS>',
         paths = 'e.g. /foo/bar   ../   ./hello\\ world/   ./src/foo.lua   ~/.config',
