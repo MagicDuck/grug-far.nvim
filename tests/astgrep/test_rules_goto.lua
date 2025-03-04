e3fa80afb68d0bdce7621293c3f88e4fc6fda5ac
Jaye Heffernan <jpeheffernan@gmail.com>
2025-03-04 22:36:25 +1100

    Copy existing test files

---
+++ tests/astgrep/test_rules_goto.lua
@@ -0,0 +1,42 @@
+local MiniTest = require('mini.test')
+local helpers = require('grug-far.test.helpers')
+local keymaps = helpers.getKeymaps()
+
+---@type NeovimChild
+local child = MiniTest.new_child_neovim()
+
+local T = MiniTest.new_set({
+  hooks = {
+    pre_case = function()
+      helpers.initChildNeovim(child)
+    end,
+    -- Stop once all test cases are finished
+    post_once = child.stop,
+  },
+})
+
+T['can open a given location'] = function()
+  helpers.writeTestFiles({
+    {
+      filename = 'file2.ts',
+      content = [[ 
+    if (grug || talks) {
+      grug.walks(talks)
+    }
+    ]],
+    },
+  })
+
+  helpers.childRunGrugFar(child, {
+    engine = 'astgrep',
+    prefills = { search = 'grug.$A' },
+  })
+  helpers.childWaitForFinishedStatus(child)
+
+  child.type_keys('<esc>9G')
+  child.type_keys('<esc>' .. keymaps.gotoLocation.n)
+  helpers.childWaitForScreenshotText(child, '3,7')
+  helpers.childExpectScreenshot(child)
+end
+
+return T
