f71f0d714e8a7d735eed6f84f4116de015361b26
MagicDuck <sbadragan@gmail.com>
2025-05-29 00:59:48 -0700

    fist try at fixing empty prefix line

--- lua/grug-far/test/screenshot.lua
+++ lua/grug-far/test/screenshot.lua
@@ -59,20 +59,20 @@ end
 function M.fromChildBufLines(child)
   local lines = child.api.nvim_buf_get_lines(0, 0, -1, true)
   -- Note: we use this if we run into random extra lines
-  local end_line = #lines
-  for i = #lines, 2, -1 do
-    if #lines[i] > 0 then
-      end_line = i
-      break
-    end
-  end
+  -- local end_line = #lines
+  -- for i = #lines, 2, -1 do
+  --   if #lines[i] > 0 then
+  --     end_line = i
+  --     break
+  --   end
+  -- end
+  --
+  -- local trimmed = {}
+  -- for i = 1, end_line, 1 do
+  --   table.insert(trimmed, lines[i])
+  -- end
 
-  local trimmed = {}
-  for i = 1, end_line, 1 do
-    table.insert(trimmed, lines[i])
-  end
-
-  return M.from_lines(trimmed)
+  return M.from_lines(lines)
 end
 
 local function case_to_stringid(case)
