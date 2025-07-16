c9292c7abdc3e5f53d8a46dc58ade7ab572462e5
MagicDuck <sbadragan@gmail.com>
2025-07-16 11:47:02 -0700

    adding tests

--- tests/astgrep/test_ui.lua
+++ tests/astgrep/test_ui.lua
@@ -1,5 +1,6 @@
 local MiniTest = require('mini.test')
 local helpers = require('grug-far.test.helpers')
+local keymaps = helpers.getKeymaps()
 
 ---@type NeovimChild
 local child = MiniTest.new_child_neovim()
@@ -18,6 +19,40 @@ T['can launch with :GrugFar astgrep'] = function()
   child.type_keys('<esc>:GrugFar astgrep<cr>')
   helpers.childWaitForScreenshotText(child, 'Search:')
   helpers.childWaitForScreenshotText(child, 'astgrep')
+end
+
+T['respects default input value when switching engine to astgrep'] = function()
+  helpers.writeTestFiles({})
+  helpers.childRunGrugFar(child, {
+    enabledEngines = { 'ripgrep', 'astgrep' },
+    prefills = {
+      search = 'grug',
+    },
+    engines = {
+      ripgrep = {
+        defaults = {
+          flags = '--smart-case --multiline',
+        },
+      },
+      astgrep = {
+        defaults = {
+          flags = '--strictness relaxed',
+        },
+      },
+    },
+  })
+  helpers.childWaitForFinishedStatus(child)
+  helpers.childExpectScreenshot(child)
+
+  -- to astgrep
+  child.type_keys('<esc>' .. keymaps.swapEngine.n)
+  helpers.childWaitForScreenshotText(child, 'astgrep')
+  helpers.childExpectScreenshot(child)
+
+  -- back to ripgrep
+  child.type_keys('<esc>' .. keymaps.swapEngine.n)
+  helpers.childWaitForScreenshotText(child, 'ripgrep')
+  helpers.childExpectScreenshot(child)
 end
 
 return T
